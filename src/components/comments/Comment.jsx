d0b100fe1b5142df659707dc1d7d596750ec2c7d
Pritam Chaudhari <pritam.chaudhari@nitorinfotech.com>
2026-09-20 14:46:13 +0530

    MUI Added

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
-import "../app/appPage.scss";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 
 const Comment = () => {
   const [comment, setComment] = useState({
@@ -15,11 +22,20 @@ const Comment = () => {
   });
 
   return (
-    <section className="unit">
-      <p className="comment-body">{comment.body}</p>
-      <p className="byline">By {comment.user.fullName}</p>
-      <span className="comment-likes">👍 {comment.likes}</span>
-    </section>
+    <Card elevation={3} sx={{ maxWidth: 800, margin: "auto", marginX: 4 }}>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {comment.body}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          By {comment.user.fullName}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <ThumbUpIcon color="action" />
+        <span>{comment.likes}</span>
+      </CardActions>
+    </Card>
   );
 };
 
--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,5 +1,12 @@
-import React, { useState } from "react";
-import "../app/appPage.scss";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 
 const Post = () => {
   const [post, setPost] = useState({
@@ -16,20 +23,23 @@ const Post = () => {
   });
 
   return (
-    <article className="post">
-      <h5>{post.title}</h5>
-      <p>{post.body}</p>
-      <div className="reactions">
-        <span>👍 {post.reactions.likes}</span>
-        <span>👎 {post.reactions.dislikes}</span>
-        <span>Views: {post.views}</span>
-      </div>
-      <div className="tags">
+    <Card elevation={3} sx={{ maxWidth: 800, margin: "auto", marginX: 4 }}>
+      <CardHeader title={post.title} subheader={`Views: ${post.views}`} />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {post.body}
+        </Typography>
         {post.tags.map((tag) => (
-          <span key={tag}>{tag}</span>
+          <Chip sx={{ m: 0.5 }} key={tag} label={tag} color="primary" />
         ))}
-      </div>
-    </article>
+      </CardContent>
+      <CardActions>
+        <ThumbUpIcon color="action" />
+        <span>{post.reactions.likes}</span>
+        <ThumbDownIcon color="action" />
+        <span>{post.reactions.dislikes}</span>
+      </CardActions>
+    </Card>
   );
 };
 
